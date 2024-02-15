a40181c70a5053c5fb75ed8b91b474e1efe2a459
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-14 21:03:21 -0500

    front3

---
+++ Warmup-1/front3.js
@@ -0,0 +1,14 @@
+// Warmup-1 -- front3
+
+// Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+// Examples
+
+// front3('Java') → JavJavJav
+// front3('Chocolate') → ChoChoCho
+
+function front3(str) {
+  let front = str.length < 3 ? str : str.substring(0, 3);
+
+  return front + front + front;
+}
