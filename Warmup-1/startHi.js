85e252b609ddb3495ea2aad991d82c395d8299d8
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-14 21:17:11 -0500

    solved startHi

---
+++ Warmup-1/startHi.js
@@ -0,0 +1,13 @@
+// Warmup-1 -- startHi
+
+// Given a string, return true if the string starts with 'hi'and false otherwise.
+
+// Examples
+
+// startHi('hi there') → true
+// startHi('hi') → true
+// startHi('hello hi') → false
+
+function startHi(str){
+  return str.startsWith('hi') ? true : false;
+}
