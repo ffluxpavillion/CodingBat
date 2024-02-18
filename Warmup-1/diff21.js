dc105f6e5517e59d25d10fe5d0c384f879e5d6b6
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-18 11:04:25 -0500

    refactored solution

--- Warmup-1/diff21.js
+++ Warmup-1/diff21.js
@@ -9,10 +9,9 @@
 // diff21(21) → 0
 
 function diff21(n){
-  if (n >= 21) {
-    return (-1*(2*(21 - n)));
-  }
-  else {
+  if (n <= 21) {
     return (21 - n);
+  } else {
+    return (n - 21)*2;
   }
 }
