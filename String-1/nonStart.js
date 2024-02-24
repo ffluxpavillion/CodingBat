4f79c64d533ddedea3eb5c8c0f27025f79bbf788
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-24 15:16:29 -0500

    solved nonStart

--- String-1/nonStart.js
+++ String-1/nonStart.js
@@ -9,5 +9,5 @@
 // nonStart('shotl', 'java') → hotlava
 
 function nonStart(a, b){
-  return a.substring(1, a.length) + b.substring(1, b.length)
+  return a.substring(1, a.length) + b.substring(1, b.length);
 }
