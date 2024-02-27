6555cc2e3bf3f5fba542c1c25a21ca91af84f627
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-27 18:14:35 -0500

    solved conCat

---
+++ String-1/conCat.js
@@ -0,0 +1,20 @@
+// String-1 -- conCat
+
+// Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+// Examples
+
+// conCat('abc', 'cat') → abcat
+// conCat('dog', 'cat') → dogcat
+// conCat('abc', '') → abc
+
+function conCat(a, b){
+  let doubleChar = a.charAt(a.length - 1) === b.charAt(0);
+
+  if (doubleChar){
+    return a + b.substring(1, b.length);
+  }
+  else {
+    return a + b;
+  }
+}
