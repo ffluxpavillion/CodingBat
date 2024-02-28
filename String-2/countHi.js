586ef8012e36a510d22f07a1d3ad6b8342d02a5d
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-28 14:56:25 -0500

    solved countHi

---
+++ String-2/countHi.js
@@ -0,0 +1,20 @@
+// String-2 -- countHi
+
+// Return the number of times that the string "hi" appears anywhere in the given string.
+
+// Examples
+
+// countHi('abc hi ho') → 1
+// countHi('ABChi hi') → 2
+// countHi('hihi') → 2
+
+function countHi(str){
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.substring(i, i + 2) === 'hi') {
+      count ++;
+    }
+  }
+  return count;
+}
