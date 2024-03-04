5ef8cef20a914fcfedb64a07847dd0c5d82087bb
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-03-04 17:21:12 -0500

    solved countYZ

---
+++ String-3/countYZ.js
@@ -0,0 +1,21 @@
+// String-3 -- countYZ
+
+// Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+
+// Examples
+
+// countYZ('fez day') → 2
+// countYZ('day fez') → 2
+// countYZ('day fyyyz') → 2
+
+function countYZ(str){
+  let count = 0;
+  str = str.toLowerCase() + ' ';
+
+  for (let i = 0; i < str.length - 1; i++){
+    if ((str[i] === 'y' || str[i] === 'z') && !(/[a-z]/i.test(str[i + 1]))){
+      count++;
+    }
+  }
+  return count;
+}
