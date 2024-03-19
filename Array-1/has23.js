11895a2269eb38297eacca6e792004ad71b2a980
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-03-18 21:20:10 -0400

    solved has23

---
+++ Array-1/has23.js
@@ -0,0 +1,13 @@
+// Array-1 -- has23
+
+// Given an int array length 2, return true if it contains a 2 or a 3.
+
+// Examples
+
+// has23([2, 5]) → true
+// has23([4, 3]) → true
+// has23([4, 5]) → false
+
+function has23(nums){
+  return nums.includes(2) || nums.includes (3);
+}
