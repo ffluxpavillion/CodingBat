efb70a4ce7a71a9bc3956f239e38ad735f6168b7
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-03-28 12:34:48 -0400

    solved evenOdd

---
+++ Array-2/evenOdd.js
@@ -0,0 +1,37 @@
+// Array-2 -- evenOdd
+
+// Return an array that contains the exact same numbers as the given array, but rearranged so that all the even numbers come before all the odd numbers. Other than that, the numbers can be in any order. You may modify and return the given array, or make a new array.
+
+// Examples
+
+// evenOdd([1, 0, 1, 0, 0, 1, 1]) → 0,0,0,1,1,1,1
+// evenOdd([3, 3, 2]) → 2,3,3
+// evenOdd([2, 2, 2]) → 2,2,2
+
+function evenOdd(nums){
+  let evens = [];
+  let odds = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    } else {
+      odds.push(nums[i]);
+    }
+  }
+
+  return evens.concat(odds);
+}
+
+// ALT SOLUTION
+// function evenOdd(nums){
+//   return nums.sort((a, b) => {
+//    if (a % 2 === 0 && b % 2 !== 0) {
+//      return -1;
+//    } else if (a % 2 !== 0 && b % 2 === 0) {
+//      return 1;
+//    } else {
+//      return 0;
+//    }
+//  });
+// }
